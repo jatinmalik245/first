95f75a47bcfb29124855b894351cf6e0d8ed75b8
Jatin <jatinmalik245@gmail.com>
2019-10-26 08:56:12 +0100

    initial commit

---
+++ todo.js
@@ -0,0 +1,59 @@
+let add=document.getElementById("add")
+add.addEventListener("click",addTask)
+let Tasks=[];
+
+function addTask(){
+    //const input=document.getElementById("task")
+    // Tasks.push(input.value)
+    // input.value=""
+    // input.focus();
+    let task=prompt("enter the task")
+    Tasks.push(task)
+    display()
+
+}
+
+function display(){
+    const table=document.querySelector("table")
+    table.innerHTML=""
+    for(let i=0;i<Tasks.length;i++){
+        if(Tasks[i].length>2)
+        {
+            let tr=document.createElement("tr")
+            let td=document.createElement("td")
+            let tdbtn=document.createElement("td")
+            td.textContent=Tasks[i]
+            td.className="first"
+            tr.appendChild(td);
+            let btn=document.createElement("button")
+            btn.textContent="delete"
+            btn.onclick= function (){
+                deleteTask(i);
+            }
+            tdbtn.appendChild(btn)
+            tr.appendChild(tdbtn)
+            table.appendChild(tr)
+         
+        }
+        
+    }
+    if(Tasks.length===0){
+        let td = document.createElement("td")
+        td.textContent="please enter a task";
+        let table =document.querySelector("table")
+        table.appendChild(td);
+    }
+}
+
+function deleteTask(index){
+    let newTasks=Tasks.filter((val,i)=>{
+        if(i===index){
+            return false;
+        }
+        else
+        return true 
+    })
+    Tasks=newTasks
+    display()
+}
+
